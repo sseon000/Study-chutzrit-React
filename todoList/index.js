247ff7bd7cc10327cf7512349bcdc5ec8eff474d
bugNick <rlatjsgur000@naver.com>
2023-06-18 00:42:12 +0900

    todo-list new item object add

--- todoList/index.js
+++ todoList/index.js
@@ -1,14 +1,18 @@
 /*============== DOM selector ======================================*/
-const todoInput = document.querySelector('.todo-input');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoUl = document.querySelector(".todo-ul");
+const todoBtn = document.querySelector(".todo-btn");
 //console.log(todoInput);
 //console.log(todoList);
+//console.log(todoBtn);
 
 /*============== add event  =========================================*/
-todoInput.addEventListener('keydown', onPressEnter);
+todoInput.addEventListener("keydown", onPressEnter);
+todoBtn.addEventListener("click", onClickTodoBtn);
 
 /*============== system variables ==================================*/
-let todoContent = '';
+let todoContent = "";
+let todoList = [];
 
 /*============== user script =======================================*/
 /** 2023.06.16 KSH
@@ -17,16 +21,45 @@ let todoContent = '';
  * [params] : none
  */
 function onPressEnter() {
-    //1. 엔터키 코드 확인
+  //console.log(window.event.keyCode);
+  // 1. todo값 가져오기
+  todoContent = todoInput.value;
+  // 2. 할 일을 적고 엔터키를 눌렀을 때, 할 일 추가
+  if (window.event.keyCode === 13 && !!todoContent.trim()) {
+    createTodo();
+    addTodo();
+  }
+}
 
-    //2. 할 일 값 가져오기
-    //console.log(todoInput.value);
-    todoContent = todoInput.value;
-    //console.log(window.event.keyCode);
-    //3. 빈 값이 아닐경우 할 일 추가
-    if(window.event.keyCode === 13 && !!todoContent) {
-        addTodo();
-    }
+/** 2023.06.18 KSH
+ * todo-btn.onClick
+ * [description] : 할 일을 적고 Add 버튼 클릭 시, 할 일 추가
+ * [params] : none
+ */
+function onClickTodoBtn() {
+  // 1. todo값 가져오기
+  todoContent = todoInput.value;
+  // 2. 할 일을 적고 엔터키를 눌렀을 때, 할 일 추가
+  if (todoContent.trim()) {
+    createTodo();
+    addTodo();
+  }
+}
+
+/** 2023.06.18 KSH
+ * createTodo
+ * [description] : 새로운 할 일 객체 만들기
+ * [params] : none
+ */
+function createTodo() {
+  // 1. new Todo 객체 생성
+  const newItem = {
+    id: todoList.length + 1,
+    todo: todoContent,
+    isComplete: false,
+  };
+  // 2. todoList 배열에 newItem 추가
+  todoList = [...todoList, newItem];
 }
 
 /** 2023.06.16 KSH
@@ -35,28 +68,23 @@ function onPressEnter() {
  * [params] : none
  */
 function addTodo() {
-    //1. span, li tag 생성
-    const newSpan = document.createElement('span');
-    const newLi = document.createElement('li');
-    const newBtn = document.createElement('button');
-    //2. btn 태그에 클릭 이벤트 연결
-    newBtn.addEventListener('click', () => {
-        //console.log('btn click')
-        newLi.classList.toggle('complete');
-        newBtn.classList.toggle('btn-complete');
-    });
-    //3. li 태그에 더블 클릭 이벤트 연결
-    newLi.addEventListener('dblclick', () => {
-        //console.log('li dblclick')
-        newLi.remove();
-    });
-    //4. span tag innerText에 할 일 값 할당
-    newSpan.innerText = todoContent;
-    //5. li tag에 span, button tag 자식 추가
-    newLi.appendChild(newBtn);
-    newLi.appendChild(newSpan);
-    //6. ul tag에 span tag 자식 추가
-    todoList.appendChild(newLi);
-    //7. input value 비우기
-    todoInput.value = '';
+  // 1. span, li tag 생성
+  const newSpan = document.createElement("span");
+  const newLi = document.createElement("li");
+  const newBtn = document.createElement("button");
+  // 2. btn 태그에 클릭 이벤트 연결
+  newBtn.addEventListener("click", () => {
+    //console.log('btn click')
+    newLi.classList.toggle("complete");
+    newBtn.classList.toggle("btn-complete");
+  });
+  // 3. span tag innerText에 할 일 값 할당
+  newSpan.innerText = todoContent;
+  // 4. li tag에 span, button tag 자식 추가
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  // 5. ul tag에 span tag 자식 추가
+  todoUl.appendChild(newLi);
+  // 6. input value 초기화
+  todoInput.value = "";
 }
